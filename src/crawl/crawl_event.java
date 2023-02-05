f3f42b4bb18a353c0895755c93cf3b165940b4b6
dominhkhoi2002 <khoi.dominh2306@gmail.com>
2023-02-05 12:06:28 +0700

    Crawl_Event

--- src/crawl/crawl_event.java
+++ src/crawl/crawl_event.java
@@ -1,5 +1,78 @@
 package crawl;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+
+import models.Event;
+
 public class crawl_event {
+    public static void main(String[] args) throws Exception {
+        String url = "https://vi.wikipedia.org/wiki/Niên_biểu_lịch_sử_Việt_Nam";
+        Document doc = Jsoup.connect(url).get();
+
+        Elements pTag = doc.select("p");
+        Elements ddTag = doc.select("dd");
+        List <Event> EventList = new ArrayList<Event>();
+        
+        pTag.remove(0);
+
+        for(Element p:pTag) {
+            try{
+                Element timeTag = p.child(0);
+                Element contentTag = p.clone();
+                contentTag.childNode(0).remove();
+                if (contentTag.text().equals("")){
+                    continue;
+                }
+                Event event = new Event(timeTag.text(), contentTag.text());
+                EventList.add(event);
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        for(Element dd: ddTag){
+            try{
+                Element timeTag = dd.child(0);
+                Element contentTag = dd.clone();
+                contentTag.childNode(0).remove();
+                Event event = new Event(timeTag.text(), contentTag.text());
+                EventList.add(event);
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
     
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String Str_Event = gson.toJson(EventList);
+        Path path = Paths.get("Events.json");
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+            writer.append(Str_Event);
+            writer.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
+
